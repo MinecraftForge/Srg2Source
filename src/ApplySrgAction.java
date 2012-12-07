84d7fe69b19d2d45a4eac97df411d5504f541577
Agaricus <agaricusb@yahoo.com>
2012-12-07 15:00:10 -0800

    Show count of PsiElements in project directory

--- src/ApplySrgAction.java
+++ src/ApplySrgAction.java
@@ -1,8 +1,22 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.module.Module;
+
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.refactoring.RenameRefactoring;
+import com.intellij.usageView.UsageInfo;
+import com.intellij.util.FileContentUtil;
+
+import java.util.Collection;
+import java.util.Set;
 
 public class ApplySrgAction extends AnAction {
     public ApplySrgAction() {
@@ -10,9 +24,27 @@ public class ApplySrgAction extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent event) {
-        // example from http://confluence.jetbrains.net/display/IDEADEV/Getting+Started+with+Plugin+Development
         Project project = event.getData(PlatformDataKeys.PROJECT);
-        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+
+        Module[] modules = ModuleManager.getInstance(project).getModules();
+
+        VirtualFile file = project.getBaseDir();
+
+
+        PsiManager psiManager = PsiManager.getInstance(project);
+        PsiDirectory psiDirectory = psiManager.findDirectory(file);
+
+        PsiElement[] psiElements = psiDirectory.getChildren();
+
+        Messages.showMessageDialog(project, "Project file: " + file.getPath() + ", modules: " + modules.length + ", elements=" + psiElements.length, "Information", Messages.getInformationIcon());
+
+        // TODO RenameRefactoring
+
+        /*  TODO: get filename of .srg
+        String txt = Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
         Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+        */
+
+        // TODO: apply
     }
 }
