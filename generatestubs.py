8ac94fee2dd85069e94e6e0fd277f32a436e77cb
Agaricus <agaricusb@yahoo.com>
2012-12-07 22:55:21 -0800

    Write stubs to disk. Not yet completely compileable.

--- generatestubs.py
+++ generatestubs.py
@@ -6,6 +6,7 @@ import os
 CB_CLASS_LIST = "../forge/classes-patched-cb-mcdev"
 ALL_CLASS_LIST_SRG = "../jars/cb2mcp.srg"
 MC_DEV_EXTRACTED_DIR = "../jars/mc-dev"
+OUT_STUB_DIR = "../CraftBukkit/src/main/java/"
 
 def getPatchedByCB():
     return set([x.strip() for x in file(CB_CLASS_LIST).readlines()])
@@ -24,17 +25,44 @@ def getAll():
 
 unpatched = getAll() - getPatchedByCB()
 for cls in sorted(list(unpatched)):
+    filename = OUT_STUB_DIR + cls + ".java"
+    if os.path.exists(filename):
+        #print "File already exists:",filename
+        #raise SystemExit
+        pass
+
+    if not os.path.exists(os.path.dirname(filename)):
+        # org/ dirs need to be created; CB only has net/
+        #os.mkdir(os.path.dirname(filename)) # need recursive mkdir
+        os.system("mkdir -p " + os.path.dirname(filename)) # warning: injection
+    
+    print filename
+    f = file(filename, "w")
+
     header = """// Auto-generated methods stubs for %s
+
+package net.minecraft.server;
+
 import org.apache.commons.lang.NotImplementedException;
 """ % (cls, )
 
+    f.write(header)
+
     lines = subprocess.Popen(["javap", "-classpath", MC_DEV_EXTRACTED_DIR, cls], stdout=subprocess.PIPE).communicate()[0].split("\n")
 
-    print header
+    if "Compiled" in lines[0]:
+        lines = lines[1:] # skip initial "Compiled from" line, if present
 
-    for line in lines[1:]: # skip initial "Compiled from" line
-        line = line.replace("net.minecraft.server.", "")
+    for line in lines:
+        line = line.replace("net.minecraft.server.", "")  # already in package
 
+        line = line.replace(" final ", " ") # 
+
+        if "{}" in line:
+            # Skip static initializer (always empty)
+            continue
+
+        # Methods
         if ")" in line:
             parts = line.split("(")
             retn = parts[0]
@@ -48,5 +76,6 @@ import org.apache.commons.lang.NotImplementedException;
             line = retn + "(" + ", ".join(namedArgs) + ") { throw new NotImplementedException(); }"
 
 
-        print line
+        f.write(line + "\n")
+    f.close()
 
