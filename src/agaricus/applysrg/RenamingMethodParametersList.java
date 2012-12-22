7264dc0475624a9cabed9e8777bf2c158c1a1cf9
Agaricus <agaricusb@yahoo.com>
2012-12-22 15:14:04 -0800

    RenamingMethodParametersList toString()

--- src/agaricus/applysrg/RenamingMethodParametersList.java
+++ src/agaricus/applysrg/RenamingMethodParametersList.java
@@ -12,4 +12,13 @@ public class RenamingMethodParametersList extends Renaming {
         this.methodSignature = methodSignature;
         this.newParameterNames = newParameterNames;
     }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("MethodParametersList "+className+" "+methodName+" "+methodSignature+" ");
+        for (String newParameterName: newParameterNames) {
+            sb.append(newParameterName);
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
 }
