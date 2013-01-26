89b13bb3184c330d3ee64453139d597d223d2e6f
Agaricus <agaricusb@yahoo.com>
2013-01-26 04:01:39 -0800

    Finish porting remainder of remap-craftbukkit.sh

--- python/remap-craftbukkit.py
+++ python/remap-craftbukkit.py
@@ -38,64 +38,72 @@ SRG_CB2MCP=os.path.join(DATA, MCVER, "cb2pkgmcp.srg")
 SRG_CB2MCP_FIXES=os.path.join(DATA, "1.4.6", "uncollide-cb2pkgmcp.srg")
 
 
-# Small fixes to accomodate renaming compatibility
-status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, MCVER, "prerenamefixes.patch"))
-assert status == 0, "failed to patch prerenamefixes"
+shouldPrepare = True
+shouldExtract = True
+shouldRename = True
+shouldFormat = True
 
-# Change minecraft-server library to a "slimmed" version without the same NMS classes CB patches
-# This avoids Psi symbol resolving errors
-status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, "pom-slim-minecraft-server.patch"))
-assert status == 0, "failed to patch pom-slim"
+if shouldPrepare:
+    # Small fixes to accomodate renaming compatibility
+    status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, MCVER, "prerenamefixes.patch"))
+    assert status == 0, "failed to patch prerenamefixes"
 
-# Preflight IDEA with the updated pom, giving it a time to scan the symbols
-cookie = os.path.join(CB_ROOT, "srg2source-batchmode")
-if os.path.exists(cookie):
-    os.unlink(cookie)
+    # Change minecraft-server library to a "slimmed" version without the same NMS classes CB patches
+    # This avoids Psi symbol resolving errors
+    status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, "pom-slim-minecraft-server.patch"))
+    assert status == 0, "failed to patch pom-slim"
 
-# Extract map of symbol ranges in CB source, required for renaming
-# IDEA must have Srg2source plugin installed, it will detect batchmode and automatically run
-CB_RANGEMAP=os.path.join(CB_ROOT, "craftbukkit.rangemap")
+if shouldExtract:
+    # Enable batch mode for Srg2source, so it runs automatically and then exits
+    cookie = os.path.join(CB_ROOT, "srg2source-batchmode")
+    if os.path.exists(cookie):
+        os.unlink(cookie)
 
-if os.path.exists(CB_RANGEMAP):
-    shutil.move(CB_RANGEMAP, CB_RANGEMAP+".old")
+    # Extract map of symbol ranges in CB source, required for renaming
+    # IDEA must have Srg2source plugin installed, it will detect batchmode and automatically run
+    CB_RANGEMAP=os.path.join(CB_ROOT, "craftbukkit.rangemap")
+
+    if os.path.exists(CB_RANGEMAP):
+        shutil.move(CB_RANGEMAP, CB_RANGEMAP+".old")
 
 
-file(cookie, "w")
+    file(cookie, "w")
 
-status = os.system(IDEA+" "+os.path.join(os.getcwd(), CB_ROOT))
-assert status == 0
+    status = os.system(IDEA+" "+os.path.join(os.getcwd(), CB_ROOT))
+    assert status == 0
 
-os.unlink(os.path.join(CB_ROOT, "srg2source-batchmode"))
+    os.unlink(os.path.join(CB_ROOT, "srg2source-batchmode"))
 
-if not os.path.exists(CB_RANGEMAP):
-    raise Exception("Failed to extract CB rangemap")
+    if not os.path.exists(CB_RANGEMAP):
+        raise Exception("Failed to extract CB rangemap")
 
-os.system("diff -ur "+CB_RANGEMAP+".old "+CB_RANGEMAP+"|wc -l")
+    os.system("diff -ur "+CB_RANGEMAP+".old "+CB_RANGEMAP+"|wc -l")
 
-# Change to a new minecraft-server library with MCP names
-status = os.system("patch -p1 -d "+CB_ROOT+" -R < "+os.path.join(DATA, "pom-slim-minecraft-server.patch"))
-assert status ==  0
+if shouldRename:
+    # Change to a new minecraft-server library with MCP names
+    status = os.system("patch -p1 -d "+CB_ROOT+" -R < "+os.path.join(DATA, "pom-slim-minecraft-server.patch"))
+    assert status ==  0
 
-status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, "pom-minecraft-server-pkgmcp.patch"))
-assert status == 0
+    status = os.system("patch -p1 -d "+CB_ROOT+" < "+os.path.join(DATA, "pom-minecraft-server-pkgmcp.patch"))
+    assert status == 0
 
-# Apply the renames
-status = os.system(PYTHON+" rangeapply.py --srcRoot "+CB_ROOT+" --srcRangeMap "+CB_RANGEMAP+" --lvRangeMap "+MCP_RANGEMAP+" --mcpConfDir "+os.path.join(MCP_ROOT, "conf")+" --srgFiles "+SRG_CB2MCP+" "+SRG_CB2MCP_FIXES)
-assert status == 0
+    # Apply the renames
+    status = os.system(PYTHON+" rangeapply.py --srcRoot "+CB_ROOT+" --srcRangeMap "+CB_RANGEMAP+" --lvRangeMap "+MCP_RANGEMAP+" --mcpConfDir "+os.path.join(MCP_ROOT, "conf")+" --srgFiles "+SRG_CB2MCP+" "+SRG_CB2MCP_FIXES)
+    assert status == 0
 
-# TODO
-# Reformat source style in NMS (only; not OBC) to more closely resemble MCP
-# This assumes you have astyle installed and ~/.astylerc copied from conf/astyle.cfg
-#ARTISTIC_STYLE_OPTIONS=../mcp726-pkgd/conf/astyle.cfg astyle --suffix=none -R $CB_ROOT/src/main/java/net/minecraft # TODO
-#find $CB_ROOT/src/main/java/net/minecraft -name '*.java' -exec astyle --suffix=none {} \;
+if shouldFormat:
+    # Reformat source style in NMS (only; not OBC) to more closely resemble MCP
+    # This assumes you have astyle installed and ~/.astylerc copied from conf/astyle.cfg
+    #ARTISTIC_STYLE_OPTIONS=../mcp726-pkgd/conf/astyle.cfg astyle --suffix=none -R $CB_ROOT/src/main/java/net/minecraft # TODO
+    #find $CB_ROOT/src/main/java/net/minecraft -name '*.java' -exec astyle --suffix=none {} \;
+    os.system("find "+os.path.join(CB_ROOT, "src/main/java/net/minecraft")+" -name '*.java' -exec astyle --suffix=none {} \;")
 
-#$PYTHON javadocxfer.py $CB_ROOT $MCP_ROOT
+    os.system(PYTHON+" javadocxfer.py "+CB_ROOT+" "+MCP_ROOT)
 
-#$PYTHON whitespaceneutralizer.py $CB_ROOT $MCP_ROOT
+    os.system(PYTHON+" whitespaceneutralizer.py "+CB_ROOT+" "+MCP_ROOT)
 
-# Measure differences to get a sense of progress
-#(diff -ur $MCP_ROOT/src/minecraft_server/net/minecraft/ $CB_ROOT/src/main/java/net/minecraft/ > $DIFF_OUT) || true
+    # Measure differences to get a sense of progress
+    os.system("diff -ur "+os.path.join(MCP_ROOT,"src/minecraft_server/net/minecraft/")+" "+os.path.join(CB_ROOT, "src/main/java/net/minecraft/")+" > "+DIFF_OUT)
 
-#wc -l $DIFF_OUT
-
+    print len(file(DIFF_OUT).readlines())
 
