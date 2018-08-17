4738d2a4ee87a0864ce35aaaff19a879c4a9d59c
LexManos <LexManos@gmail.com>
2018-08-16 21:30:35 -0700

    Fix tsrg current class and support lists of strings.

--- src/main/java/net/minecraftforge/srg2source/rangeapplier/SrgContainer.java
+++ src/main/java/net/minecraftforge/srg2source/rangeapplier/SrgContainer.java
@@ -37,70 +37,76 @@ public class SrgContainer
     {
         try
         {
-            String currentClass = null;
-            List<String> lines = Files.readLines(srg, StandardCharsets.UTF_8);
-            lines = lines.stream().map(line -> line.split("#")[0]).filter(l -> !Strings.isNullOrEmpty(l.trim())).collect(Collectors.toList());
-
-            for (String line : lines) //Gather class remaps so we can load TSRG
-            {
-                if (line.startsWith("\t") || (line.indexOf(':') != -1 && !line.startsWith("CL:")))
-                    continue;
-                if (line.indexOf(':') != -1)
-                    line = line.substring(4);
-
-                String[] args = line.split(" ");
-                if (args.length == 2 && !args[0].endsWith("/"))
-                    classMap.put(args[0], args[1]);
-            }
-
-            for (String line : lines)
-            {
-                int idx = line.indexOf(':');
-                if (idx != -1 && line.startsWith("CL:"))
-                    continue;
-
-                String type = idx != -1 ? line.substring(0, idx) : null;
-                if (idx != -1) line = line.substring(4);
-                if (line.startsWith("\t")) {
-                    if (currentClass == null)
-                        throw new RuntimeException("Invalid TSRG line, missing current class: " + line);
-                    line = currentClass + " " + line.substring(1);
-                }
-
-                String[] args = line.split(" ");
-                if (type != null)
-                {
-                    if (type.equals("PK"))
-                        packageMap.put(args[0], args[1]);
-                    else if (type.equals("CL"))
-                        ; // We already read classes above.
-                    else if (type.equals("FD"))
-                        fieldMap.put(args[0], args[1]);
-                    else if (type.equals("MD"))
-                        methodMap.put(new MethodData(args[0], args[1]), new MethodData(args[2], args[3]));
-                    else
-                        throw new RuntimeException("Invalid SRG file: " + srg + " Line: " + type + ": " + line);
-                }
-                else
-                {
-                  if (args.length == 2)
-                  {
-                      if (args[0].endsWith("/")) //Package
-                          packageMap.put(args[0].substring(0, args[0].length() - 1), args[1].substring(0, args[1].length() - 1));
-                  }
-                  else if (args.length == 3)
-                      fieldMap.put(args[0] + "/" + args[1], remapClass(args[0]) + "/" + args[2]);
-                  else if (args.length == 4)
-                      methodMap.put(new MethodData(args[0] + "/" + args[1], args[2]), new MethodData(remapClass(args[0]) + "/" + args[3], remapDesc(args[2])));
-                  else
-                      throw new RuntimeException("Invalid SRG file: " + srg + " Line: " + type + ": " + line);
-                }
-            }
+            return readSrg(Files.readLines(srg, StandardCharsets.UTF_8), reverse);
         }
         catch (IOException e)
         {
             Throwables.throwIfUnchecked(e);
             throw new RuntimeException(e);
+        }
+    }
+
+    public SrgContainer readSrg(List<String> lines, boolean reverse)
+    {
+        String currentClass = null;
+        lines = lines.stream().map(line -> line.split("#")[0]).filter(l -> !Strings.isNullOrEmpty(l.trim())).collect(Collectors.toList());
+
+        for (String line : lines) //Gather class remaps so we can load TSRG
+        {
+            if (line.startsWith("\t") || (line.indexOf(':') != -1 && !line.startsWith("CL:")))
+                continue;
+            if (line.indexOf(':') != -1)
+                line = line.substring(4);
+
+            String[] args = line.split(" ");
+            if (args.length == 2 && !args[0].endsWith("/"))
+                classMap.put(args[0], args[1]);
+        }
+
+        for (String line : lines)
+        {
+            int idx = line.indexOf(':');
+            if (idx != -1 && line.startsWith("CL:"))
+                continue;
+
+            String type = idx != -1 ? line.substring(0, idx) : null;
+            if (idx != -1) line = line.substring(4);
+            if (line.startsWith("\t")) {
+                if (currentClass == null)
+                    throw new RuntimeException("Invalid TSRG line, missing current class: " + line);
+                line = currentClass + " " + line.substring(1);
+            }
+
+            String[] args = line.split(" ");
+            if (type != null)
+            {
+                if (type.equals("PK"))
+                    packageMap.put(args[0], args[1]);
+                else if (type.equals("CL"))
+                    ; // We already read classes above.
+                else if (type.equals("FD"))
+                    fieldMap.put(args[0], args[1]);
+                else if (type.equals("MD"))
+                    methodMap.put(new MethodData(args[0], args[1]), new MethodData(args[2], args[3]));
+                else
+                    throw new RuntimeException("Invalid SRG Line: " + type + ": " + line);
+            }
+            else
+            {
+              if (args.length == 2)
+              {
+                  if (args[0].endsWith("/")) //Package
+                      packageMap.put(args[0].substring(0, args[0].length() - 1), args[1].substring(0, args[1].length() - 1));
+                  else
+                      currentClass = args[0];
+              }
+              else if (args.length == 3)
+                  fieldMap.put(args[0] + "/" + args[1], remapClass(args[0]) + "/" + args[2]);
+              else if (args.length == 4)
+                  methodMap.put(new MethodData(args[0] + "/" + args[1], args[2]), new MethodData(remapClass(args[0]) + "/" + args[3], remapDesc(args[2])));
+              else
+                  throw new RuntimeException("Invalid CSRG Line: " + type + ": " + line);
+            }
         }
 
         return this;
